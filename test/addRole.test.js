a3bf6866f5968c1f9fdc6af7fb77741d116d2362
aKingsView <106622740+aKingsView@users.noreply.github.com>
2022-10-03 15:20:17 -0400

    file restructor

---
+++ test/addRole.test.js
@@ -0,0 +1,57 @@
+
+const promptEngineer = require('../../createTeam');
+const inquirer = require("inquirer");
+const Engineer = require('../../engineer');
+
+
+// describe('test user input' () => {
+
+//     // stub inquirer
+//     let backup;
+//     before(() => {
+//       backup = addRole();
+//       addRole(questions) => Promise.resolve({response: 'Manager'})
+//     })
+  
+//     it('should equal test', () => {
+//       module(...).then(answers => answers.response.should.equal('Manager'))
+//     })
+  
+//     // // restore
+//     // after(() => {
+//     //   inquirer.prompt = backup
+//     // })
+  
+//   })
+  
+
+// test(" Add Role Iteration", async () => {
+//     let mockResponses = {
+//         "employeeRole": "Engineer",
+//         "engineerName": "Test",
+//         "employeeEmail": "Test@test.com",
+//         "githubAccount": "TestMan",
+//         "employeeID": "15",
+
+//     };
+
+    // const inquirer = {
+    //     prompt: (questions) => {
+    //         let response = {}
+    //         questions.forEach((question) => {
+    //             response[question.engeineerName] = mockResponses[question.engineerName]
+    //         });
+    //         return Promise.resolve(response);
+    //     }
+    // };
+    // more to come
+
+//     const employee = await addRole();
+//     expect(typeof (employee)).toBe("object")
+// });
+
+test("Can get name via promptEngineer()", () => {
+    const name = "Sean";
+    const e = new Engineer(name);
+    expect(promptEngineer(e.name).toBe(name));
+});
